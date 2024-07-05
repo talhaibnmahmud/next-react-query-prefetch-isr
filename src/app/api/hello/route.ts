c26120e5aece27f158029a83b60af265c2001bb0
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-07-06 00:48:53 +0600

    feat: Update hello API route with URL logging & ISR

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,9 +1,10 @@
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 300;
 
-export const GET = async () => {
-    console.log("GET /api/hello");
+export const GET = async (req: NextRequest) => {
+    const url = new URL(req.url, "http://localhost:3000");
+    console.log(`GET ${url.pathname}`);
 
     return NextResponse.json({ message: "Hello, World!" });
 };
